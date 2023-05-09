63eb2e37abc30dcadacd528b739e846237b8d914
Julia Dikolenko <tsvetkova-julia-vl@yandex.ru>
2023-05-09 20:55:10 +0300

    добавить императивные обработчики handleEditAvatarClick, handleEditProfileClick, handleAddPlaceClick

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 function Main() {
+  function handleEditAvatarClick() {
+    document.querySelector('.popup_type_avatar').classList.add('popup_opened');
+  }
+  
+  function handleEditProfileClick() {
+    document.querySelector('.popup_type_edit').classList.add('popup_opened');
+  }
+  
+  function handleAddPlaceClick() { 
+    document.querySelector('.popup_type_add').classList.add('popup_opened');
+  }
+
   return (
     <main className="content">
       <section className="profile" aria-label="профиль пользователя">
@@ -9,7 +21,10 @@ function Main() {
             className="profile__avatar-image"
             src="#"
             alt="Аватар пользователя"/>
-          <div className="profile__avatar-edit"></div>
+          <div
+            className="profile__avatar-edit"
+            onClick={handleEditAvatarClick}>
+          </div>
         </div>
         <div className="profile__info">
           <h1 className="profile__name"></h1>
@@ -17,7 +32,8 @@ function Main() {
             className="profile__edit-button"
             type="button"
             aria-label="кнопка-редактировать"
-            title = "Редактировать">
+            title = "Редактировать"
+            onClick={handleEditProfileClick}>
           </button>
           <p className="profile__about"></p>
         </div>
@@ -25,7 +41,8 @@ function Main() {
           className="profile__add-button"
           type="button"
           aria-label="кнопка-добавить"
-          title = "Добавить карточку">
+          title = "Добавить карточку"
+          onClick={handleAddPlaceClick}>
         </button>
       </section>
       <section className="gallery" aria-label="карточки с фотографиями">
